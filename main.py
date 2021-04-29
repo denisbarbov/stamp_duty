1bb42168480192193983ca1dc65556a3b422ddc2
psydb6 <psydb6@exmail.nottingham.ac.uk>
2021-04-29 16:42:35 +0100

    capped lower threshold additional homes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 lower_threshold = 500000
 middle_threshold = 925000
 upper_threshold = 1500000
+additionalhome_min_threshold = 0
+
 
 def capping(price, current_calc):
     if price > middle_threshold and current_calc=='lower':
@@ -9,9 +11,35 @@ def capping(price, current_calc):
         return new_price
     elif price <= middle_threshold and current_calc=='lower':
         return price
+    elif price > upper_threshold and current_calc=='middle':
+        new_price = upper_threshold
+        return new_price
+    elif price > middle_threshold and price <=upper_threshold and current_calc=='middle':
+        return price
+        
+        
+def capping_additional_homes(price, current_calc):
+    if price > additionalhome_min_threshold and price < lower_threshold and current_calc=='min':
+        return price
+    elif price > middle_threshold and current_cal == 'lower':
+        new_price = middle_threshold
+        return new_price
+    elif price <= middle_threshold and current_calc == 'lower':
+        return price
+    elif price > upper_threshold and current_calc == 'middle':
+        new_price = upper_threshold
+        return new_price
+    elif price > middle_threshold and price <=upper_threshold and current_cal == 'middle':
+        return price
+        
 
-
-
+def calculateSecondHomeLowerThreshold(price):
+    percentage = 0.03
+    price_compared = capping_additional_homes(price, 'min')
+    stamp = price_compared*percentage
+    return 'You will pay £{} in stamp duty'.format(stamp)
+    
+    
 def calculateStampDutyLowerThreshold(price, first_home):
     if first_home:
         percentage = 0.05
@@ -20,18 +48,23 @@ def calculateStampDutyLowerThreshold(price, first_home):
         return stamp
     else:
         percentage = 0.08
-        stamp = (price - lower_threshold)*percentage
+        price_compared = capping_additional_homes(price, 'lower')
+        stamp = (price_compared - lower_threshold)*percentage
         return stamp
+        
         
 def calculateStampDutyMiddleThreshold(price, first_home):
     if first_home:
         percentage = 0.10
-        stamp = (price - middle_threshold)*percentage
+        price_compared = capping(price, 'middle')
+        stamp = (price_compared - middle_threshold)*percentage
         return stamp
     else:
         percentage = 0.13
-        stamp = (price - middle_threshold)*percentage
+        price_compared = capping_additional_homes(price, 'middle')
+        stamp = (price_compared - middle_threshold)*percentage
         return stamp
+        
 
 def calculateStampDutyUpperThreshold(price, first_home):
     if first_home:
@@ -42,6 +75,7 @@ def calculateStampDutyUpperThreshold(price, first_home):
         percentage = 0.15
         stamp = (price - upper_threshold)*percentage
         return stamp
+        
 
 def calculateStampDuty(price, first_home):
     if price <= lower_threshold:
@@ -53,11 +87,8 @@ def calculateStampDuty(price, first_home):
 
     if price > middle_threshold and price <= upper_threshold:
         lower_stamp = calculateStampDutyLowerThreshold(price, first_home)
-        print(lower_stamp)
         middle_stamp = calculateStampDutyMiddleThreshold(price, first_home)
-        print(middle_stamp)
         final_stamp = lower_stamp + middle_stamp
-        print(final_stamp)
         return 'You will pay £{} in stamp duty.'.format(final_stamp)
 
     if price > upper_threshold:
@@ -67,16 +98,14 @@ def calculateStampDuty(price, first_home):
         final_stamp = lower_stamp + middle_stamp + upper_stamp
         return 'You will pay £{} in stamp duty.'.format(final_stamp)
 
-def calculateSecondHomeLowerThreshold(price):
-    percentage = 0.03
-    stamp = price*percentage
-    return 'You will pay £{} in stamp duty'.format(stamp)
+
 
 def stampDutyCalculator(first_home, price):
     if not first_home and price <= lower_threshold:
         return calculateSecondHomeLowerThreshold(price)
     else:
         return calculateStampDuty(price, first_home)
+
 
 if __name__ == "__main__":
     print("Let's calculate your stamp duty")
